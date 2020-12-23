2335e4cb2e42dd96ad8a297ac218c822efb5798e
mohit tanwar <m.tanwar@varshyl.com>
2020-12-23 21:19:06 +0530

    icon updated

--- app/src/main/java/com/ezymd/restaurantapp/utils/BaseRequest.java
+++ app/src/main/java/com/ezymd/restaurantapp/utils/BaseRequest.java
@@ -15,6 +15,7 @@ public class BaseRequest {
         accessToken = userInfo.getAccessToken();
         paramsMap = new HashMap<>();
         paramsMap.put("lat", userInfo.getLat());
+        paramsMap.put("user_id", "" + userInfo.getUserID());
         paramsMap.put("device_token", userInfo.getDeviceToken());
         paramsMap.put("device_id", userInfo.getDeviceID());
         paramsMap.put("long", userInfo.getLang());
