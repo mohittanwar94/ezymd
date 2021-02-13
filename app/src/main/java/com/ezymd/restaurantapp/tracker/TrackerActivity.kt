c704cf7d951fdc4e569317c5b85437decfd93998
mohit tanwar <m.tanwar@varshyl.com>
2021-02-13 21:48:02 +0530

    reduce letter spacing

--- app/src/main/java/com/ezymd/restaurantapp/tracker/TrackerActivity.kt
+++ app/src/main/java/com/ezymd/restaurantapp/tracker/TrackerActivity.kt
@@ -726,7 +726,7 @@ class TrackerActivity : BaseActivity(), OnMapReadyCallback, SinchService.StartFa
                     )
                     movingCabMarker?.position = nextLocation
                     val heading = computeHeading(previousLatLng, nextLocation);
-                   // movingCabMarker?.rotation = heading.toFloat()
+                    movingCabMarker?.rotation = heading.toFloat() - 90
 
                     //  val rotation = MapUtils.getRotation(previousLatLng!!, nextLocation)
                     /* if (!rotation.isNaN()) {
