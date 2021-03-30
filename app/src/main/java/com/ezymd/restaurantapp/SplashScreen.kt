7f9fb3b228a92b428d55df58d29740157d0fd54f
Pratik Chatterjee <er.pratik7@gmail.com>
2021-03-31 00:23:43 +0530

    ReviewsListActivity Filter implementation

--- app/src/main/java/com/ezymd/restaurantapp/SplashScreen.kt
+++ app/src/main/java/com/ezymd/restaurantapp/SplashScreen.kt
@@ -13,7 +13,6 @@ import android.os.Bundle
 import android.util.Base64
 import android.util.Log
 import com.ezymd.restaurantapp.login.Login
-import com.ezymd.restaurantapp.reviewsList.ReviewsListActivity
 import com.ezymd.restaurantapp.utils.SnapLog
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
@@ -56,8 +55,8 @@ class SplashScreen : BaseActivity() {
         super.onResume()
         // printKeyHash(this)
         if (userInfo!!.userID != 0)
-        //startActivity(Intent(this, MainActivity::class.java))
-            startActivity(Intent(this, ReviewsListActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
+        //startActivity(Intent(this, ReviewsListActivity::class.java))
         else
             startActivity(Intent(this, Login::class.java))
 
