217d7eca22273562f5321a5902727b13e2cf7238
Pratik Chatterjee <er.pratik7@gmail.com>
2021-04-01 13:41:15 +0530

    shopid

--- app/src/main/java/com/ezymd/restaurantapp/reviewsList/ReviewsListActivity.kt
+++ app/src/main/java/com/ezymd/restaurantapp/reviewsList/ReviewsListActivity.kt
@@ -116,13 +116,13 @@ class ReviewsListActivity : BaseActivity() {
 
     private fun fetchData() {
         val baseRequest = BaseRequest(userInfo)
-        baseRequest.paramsMap["shop_id"] = "11"
+        baseRequest.paramsMap["shop_id"] = shopId.toString()
         viewModel.getShopReview(baseRequest)
     }
 
     private fun fetchData(rating: String) {
         val baseRequest = BaseRequest(userInfo)
-        baseRequest.paramsMap["shop_id"] = "11"
+        baseRequest.paramsMap["shop_id"] = shopId.toString()
         baseRequest.paramsMap["rating"] = rating
         viewModel.getShopReviewByRating(baseRequest)
     }
